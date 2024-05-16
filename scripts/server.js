f7684b67e41f45896b24db4f6d50623362765eb8
Cainan Ziemski Cho <cainan@macpost.com>
2024-05-16 13:10:04 -0700

    Add backend routes for chatbot

--- scripts/server.js
+++ scripts/server.js
@@ -1,29 +1,31 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv').config();
-const ejs = require('ejs')
-const bcrypt = require('bcrypt')
-const { Db } = require('mongodb');
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
-const nodemailer = require('nodemailer')
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const ejs = require("ejs");
+const bcrypt = require("bcrypt");
+const { Db } = require("mongodb");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const nodemailer = require("nodemailer");
 
-const app = express()
+const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 async function main() {
-  username = process.env.DB_USERNAME
-  password = process.env.DB_PASSWORD
-  host = process.env.DB_HOST
-  database = process.env.DB_DATABASE
-  await mongoose.connect(`mongodb+srv://${username}:${password}@${host}/${database}`)
+  username = process.env.DB_USERNAME;
+  password = process.env.DB_PASSWORD;
+  host = process.env.DB_HOST;
+  database = process.env.DB_DATABASE;
+  await mongoose.connect(
+    `mongodb+srv://${username}:${password}@${host}/${database}`
+  );
 
   app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running')
-  })
+    console.log("Server is running");
+  });
 }
 
 const transporter = nodemailer.createTransport({
@@ -42,15 +44,15 @@ const userSchema = new mongoose.Schema({
   phone: String,
   password: String,
   firstName: String,
-  lastName: String
-})
+  lastName: String,
+});
 
 const deviceSchema = new mongoose.Schema({
   name: String,
-})
+});
 
-const users = mongoose.model('2800users', userSchema)
-const devices = mongoose.model('devices', deviceSchema)
+const users = mongoose.model("2800users", userSchema);
+const devices = mongoose.model("devices", deviceSchema);
 
 // create the session/cookie
 app.use(
@@ -58,7 +60,9 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: `mongodb+srv://${username}:${password}@${host}/${database}` }), // Store session in MongoDB
+    store: MongoStore.create({
+      mongoUrl: `mongodb+srv://${username}:${password}@${host}/${database}`,
+    }), // Store session in MongoDB
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 1 day
     },
@@ -67,91 +71,88 @@ app.use(
 
 // change this to the homepage
 app.get("/", (req, res) => {
-  res.redirect("/signUp")
-})
+  res.redirect("/signUp");
+});
 
 app.get("/signUp", (req, res) => {
   res.render("signUp", {
-    error: req.query.error
-  })
-})
+    error: req.query.error,
+  });
+});
 
 app.post("/signUp", async (req, res) => {
   if (req.body.password == req.body.repeat_password) {
-    saltRounds = 10
-    hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
-    const user = new users({ 
-      username: req.body.username, 
-      email: req.body.email, 
-      phone: req.body.phone, 
-      password: hashedPassword, 
-      name: req.body.firstName, 
-      lastName: req.body.lastName 
+    saltRounds = 10;
+    hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+    const user = new users({
+      username: req.body.username,
+      email: req.body.email,
+      phone: req.body.phone,
+      password: hashedPassword,
+      name: req.body.firstName,
+      lastName: req.body.lastName,
     });
 
-    req.session.user = { 
-      username: req.body.username, 
-      email: req.body.email, 
-      phone: req.body.phone 
+    req.session.user = {
+      username: req.body.username,
+      email: req.body.email,
+      phone: req.body.phone,
     }; // Store user information in session
 
-    createdUser = await users.create({ 
-      username: req.body.username, 
-      email: req.body.email, 
-      phone: req.body.phone, 
-      password: hashedPassword, 
-      name: req.body.firstName, 
-      lastName: req.body.lastName 
-    })
+    createdUser = await users.create({
+      username: req.body.username,
+      email: req.body.email,
+      phone: req.body.phone,
+      password: hashedPassword,
+      name: req.body.firstName,
+      lastName: req.body.lastName,
+    });
   } else {
-    return res.redirect("/signUp?error=passwords_dont_match")
+    return res.redirect("/signUp?error=passwords_dont_match");
   }
-  res.redirect("/login")
-})
-
+  res.redirect("/login");
+});
 
 app.get("/login", (req, res) => {
-  res.render("login")
-})
+  res.render("login");
+});
 
 app.post("/login", async (req, res) => {
-  usersUsername = req.body.username
-  usersPassword = req.body.password
+  usersUsername = req.body.username;
+  usersPassword = req.body.password;
 
   // Check if the email is in the database
   try {
     const user = await users.findOne({
-      username: usersUsername
-    })
+      username: usersUsername,
+    });
 
     if (user) {
       await bcrypt.compare(usersPassword, user.password, (err, result) => {
         // Check if the entered password is the same as the stored password
         if (result) {
-          req.session.authenticated = true // authentication here
-          req.session.user = { 
-            username: user.username, 
-            email: user.email, 
-            phone: user.phone 
+          req.session.authenticated = true; // authentication here
+          req.session.user = {
+            username: user.username,
+            email: user.email,
+            phone: user.phone,
           }; // Store user information in session
-          return res.redirect("homePage")
+          return res.redirect("homePage");
         } else {
-          res.status(401).send("Invalid password")
+          res.status(401).send("Invalid password");
         }
-      })
+      });
     } else {
-      res.status(401).send("User not found")
+      res.status(401).send("User not found");
     }
+  } catch (err) {
+    return res.status(500).send("Server error page here. Status code 500"); // change
   }
-  catch (err) {
-    return res.status(500).send("Server error page here. Status code 500") // change
-  }
-})
+});
 
-
-app.post('/logout', (req, res) => {
+app.post("/logout", (req, res) => {
   req.session.destroy();
-  res.redirect('/index');
+  res.redirect("/index");
 });
 
 // middleware for authentication check
@@ -159,18 +160,18 @@ function isAuthenticated(req, res, next) {
   if (req.session.authenticated) {
     return next();
   }
-  res.redirect('/login');
+  res.redirect("/login");
 }
 
 // now a protected route route
-app.get('/homePage', isAuthenticated, (req, res) => {
+app.get("/homePage", isAuthenticated, (req, res) => {
   res.send(`some home page here`);
 });
 
 // the password recovery route
 app.get("/recovery", (req, res) => {
-  res.render("recovery")
-})
+  res.render("recovery");
+});
 
 // the password recovery form post route
 app.post("/recovery", async (req, res) => {
@@ -179,12 +180,12 @@ app.post("/recovery", async (req, res) => {
     //find user with username and email
     const userForRecovery = await users.findOne({
       username: req.body.username,
-      email: req.body.email
-    })
+      email: req.body.email,
+    });
     // if the user is found
     if (userForRecovery) {
       // generate a random code
-      let randomCode = Math.floor(100000 + Math.random() * 900000)
+      let randomCode = Math.floor(100000 + Math.random() * 900000);
       // send the email
       await transporter.sendMail({
         // from harmonia gmail account
@@ -196,22 +197,22 @@ app.post("/recovery", async (req, res) => {
         //plain text body
         text: `Your recovery code is ${randomCode}`,
         // html body
-        html: `<p>Your recovery code is <b>${randomCode}</b></p>`
+        html: `<p>Your recovery code is <b>${randomCode}</b></p>`,
       });
       //render newPassword page and send info to change password
       res.render("newPassword", {
         username: req.body.username,
         email: req.body.email,
         phone: "",
-        code: randomCode
-      })
+        code: randomCode,
+      });
       //if no user is found
     } else {
       // send a message to the user
-      return res.send("User not found")
+      return res.send("User not found");
     }
   }
-})
+});
 
 // the password recovery form post route
 app.post("/replacePassword", async (req, res) => {
@@ -220,39 +221,38 @@ app.post("/replacePassword", async (req, res) => {
     //find user with username and email
     const userForPasswordChange = await users.findOne({
       username: req.body.username,
-      email: req.body.email
-    })
+      email: req.body.email,
+    });
     // salt rounds to hash new password
-    saltRounds = 10
+    saltRounds = 10;
     // hash the new password
-    hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
+    hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
     // update the user's password
-    await userForPasswordChange.updateOne({ password: hashedPassword })
+    await userForPasswordChange.updateOne({ password: hashedPassword });
     // redirect the user to the login page
-    return res.redirect("/login")
+    return res.redirect("/login");
   }
   if (req.body.phone) {
     //find user with username and email
     const userForPasswordChange = await users.findOne({
       username: req.body.username,
-      phone: req.body.phone
-    })
+      phone: req.body.phone,
+    });
     // salt rounds to hash new password
-    saltRounds = 10
+    saltRounds = 10;
     // hash the new password
-    hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
+    hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
     // update the user's password
-    await userForPasswordChange.updateOne({ password: hashedPassword })
+    await userForPasswordChange.updateOne({ password: hashedPassword });
     // redirect the user to the login page
-    return res.redirect("/login")
+    return res.redirect("/login");
   }
-})
+});
 
-app.get('/profile', isAuthenticated, (req, res) => {
-
+app.get("/profile", isAuthenticated, (req, res) => {
   if (!req.session.authenticated) {
     // If the user is not authenticated, redirect them to the home page
-    res.redirect('/login');
+    res.redirect("/login");
     return;
   }
 
@@ -262,9 +262,22 @@ app.get('/profile', isAuthenticated, (req, res) => {
   const userEmail = req.session.user.email;
   const userPhone = req.session.user.phone;
   // const userPhonenumber = req.session.user.phonenumber;
-  res.render('profilePage', { 
-    userName, 
-    userEmail, 
-    userPhone });
+  res.render("profilePage", {
+    userName,
+    userEmail,
+    userPhone,
+  });
 });
 
+allUsersMessages = [];
+
+// add isauthenticated
+app.post("/sendMessage", (req, res) => {
+  allUsersMessages.push(req.body.message);
+  res.redirect("/harmonia-dm");
+});
+
+app.get("/harmonia-dm", (req, res) => {
+  // add isauthenticated
+  res.render("chatbot", { allUsersMessages: allUsersMessages });
+});
